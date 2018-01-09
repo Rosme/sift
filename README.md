df7cd8cb2ca63b4295223ad09879dbce5a416995
Redyz <Redyz>
2018-01-08 20:07:44 -0500

    Readme + gitignore

---
+++ README.md
@@ -0,0 +1,12 @@
+# TBD PFE 3000
+TBD PFE is a great project that involves c++ and aims to be a great project.
+# Building
+* mkdir build
+* cd build
+* cmake ..
+* cmake --build .
+# Conventions
+* Indentation is 2 spaces
+* Bracket style is [1TBS](https://en.wikipedia.org/wiki/Indentation_style#1TBS)
+ * _if_ with just 1 instruction still needs brackets
+* macros should be avoided
--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,16 @@
 
 
 int main(int argc, char* argv[]) {
-	
-	nlohmann::json json;
+  nlohmann::json json;
 
-	json["type"] = "Welcome";
-	json["value"] = std::vector<std::string>({ "Hello", "World" });
+  json["type"] = "Welcome";
+  json["value"] = std::vector<std::string>({ "Hello", "World" });
 
-    Core::write_json_file("testFile.txt", json);
-    
-    auto readJson = Core::read_json_file("testFile.txt");
+  Core::write_json_file("testFile.txt", json);
+  
+  auto readJson = Core::read_json_file("testFile.txt");
 
-    std::cout << readJson;
-
-    std::cin.get();
-	return 0;
+  std::cout << readJson;
+  std::cin.get();
+  return 0;
 }
