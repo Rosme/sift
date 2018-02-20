6eb23f744158845193f0999fbcc2bf647320d626
Red <red@red.com>
2018-02-20 23:19:52 +0000

    Fixed system tests under windows

--- tests/tests_system.cpp
+++ tests/tests_system.cpp
@@ -32,9 +32,9 @@
 #include <fstream>
 
 #ifdef WIN32
-#define BIN_NAME "PFE.exe"
+  #define BIN_NAME "PFE.exe"
 #else
-#define BIN_NAME "PFE"
+  #define BIN_NAME "PFE"
 #endif
 
 #define TESTS_DIR "temp-system-tests/"
@@ -60,7 +60,7 @@ void runTest(std::string args, int& outCode, std::vector<std::string>& outOutput
   std::string outputFilename(TESTS_DIR + std::to_string(currentFileNumber));
   std::string outputStdoutFilename(outputFilename + "_stdout");
   std::stringstream st;
-  st << "../" << BIN_NAME << " " << args << " -o " << outputFilename << " > " << outputStdoutFilename;
+  st << BIN_NAME << " " << args << " -o " << outputFilename << " > " << outputStdoutFilename;
   std::cout << "Running: " << st.str() << std::endl;
   outCode = std::system(st.str().c_str());
   ++currentFileNumber;
@@ -68,35 +68,39 @@ void runTest(std::string args, int& outCode, std::vector<std::string>& outOutput
   std::ifstream fileOutput(outputFilename);
   std::ifstream fileStdout(outputStdoutFilename);
   
-  PFE_ASSERT(fileOutput.is_open(), "Could not open output file for reading");
-  PFE_ASSERT(fileStdout.is_open(), "Could not open stdout file for reading");
-  
   std::string line;
-  while(std::getline(fileOutput, line)) {
-    outOutput.push_back(line);
+  if (fileOutput.is_open())
+  {
+    while (std::getline(fileOutput, line)) {
+      outOutput.push_back(line);
+    }
   }
-  
-  while(std::getline(fileStdout, line)) {
-    outStdout.push_back(line);
+
+  if (fileStdout.is_open())
+  {
+    while (std::getline(fileStdout, line)) {
+      outStdout.push_back(line);
+    }
   }
 }
 
 TEST_CASE("Testing sift itself like a blackbox", "[system]") {
   int code = -1;
-  std::vector<std::string> output;
-  std::vector<std::string> stdout;
+  std::vector<std::string> outputVector;
+  std::vector<std::string> stdoutVector;
   
   SECTION("All green quiet test") {
-    runTest("-q -r samples/tests/rules/empty.json -p samples/src/define.hpp", code, output, stdout);
+    runTest("-q -r samples/tests/rules/empty.json -p samples/src/define.hpp", code, outputVector, stdoutVector);
     REQUIRE(code == 0);
-    REQUIRE_OUTPUT_SIZE(stdout, 0);
-    REQUIRE_OUTPUT_SIZE(output, 0);
+    REQUIRE_OUTPUT_SIZE(stdoutVector, 0);
+    REQUIRE_OUTPUT_SIZE(outputVector, 0);
   }
   
   SECTION("Run sift on itself with no errors") {
-    runTest("-q -r samples/tests/rules/empty.json -p ../../src", code, output, stdout);
+    runTest("-q -r samples/tests/rules/empty.json -p samples/SIFT", code, outputVector, stdoutVector);
     REQUIRE(code == 0);
-    REQUIRE_OUTPUT_SIZE(stdout, 0);
-    REQUIRE_OUTPUT_SIZE(output, 0);
+    REQUIRE_OUTPUT_SIZE(stdoutVector, 0);
+    REQUIRE_OUTPUT_SIZE(outputVector, 0);
   }
+
 }
